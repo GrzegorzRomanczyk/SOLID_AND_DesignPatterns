5174e4162eb0b522c6104335485919758205ec7f
GrzegorzRomanczyk <baczekexp@gmail.com>
2023-06-08 23:27:08 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_AND_DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
